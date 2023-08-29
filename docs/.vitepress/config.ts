68672e10bf82c8e1a3d7d4d0c7a84606a0ab6db4
jyj <jyj@163.com>
2023-08-29 14:42:23 +0800

    fix: change base path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vitepress';
 
 // refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
-  lang: 'en-US',
+  // lang: 'en-US',
   title: 'Common admin template',
   description: 'Vite & Vue powered static site generator.',
 
   // config source directory
   srcDir: './src',
   // https://vitepress.dev/guide/deploy#setting-a-public-base-path
-  base: '/base/',
+  base: '/docs/',
 
   themeConfig: {
     nav: [
